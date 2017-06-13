11a08a8132e9cf8a6ec7c539a562334b6c059b5b
Naoto Shimazaki <nshimaza@cisco.com>
2017-06-14 06:59:13 +0900

    added haddock annotations

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # tcp-server
-Tiny TCP server framework
+A Simple Plain TCP Server.
+
+Boot a simple, plain, general purpose TCP server with automatic hierarchical thread management.
+It accepts a user provided handler and forks dedicated thread which listens given TCP port number.
+Every time the listening thread accept a new connection from peer, it forks new thread dedicated for
+each connection then calls user provided handler.
+
+When server shutdown, all threads including listening thread, handler threads and their children
+automatically killed.  You don't have to close the socket by your own.  This module automatically
+close the socket on handler exit regardless if it exited normally or it was killed.
+You can install your own cleanup routine using finally or bracket.
+
+
+### Example
+
+```haskell
+newEchoServer :: IO TcpServer
+newEchoServer = newServer 8080 echoServerHandler
+
+echoServerHandler :: ThreadMap -> Socket -> IO ()
+echoServerHandler _ peer = go
+  where
+    go = do
+        msg <- recv peer 4096
+        unless (null msg) $ do
+            sendAll peer msg
+            go
+```
--- src/Network/TcpServer.hs
+++ src/Network/TcpServer.hs
@@ -1,11 +1,51 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-|
+Module      : Network.TcpServer
+Copyright   : (c) Naoto Shimazaki 2017
+License     : MIT (see the file LICENSE)
 
+Maintainer  : https://github.com/nshimaza
+Stability   : experimental
+
+A Simple Plain TCP Server.
+
+Boot a simple, plain, general purpose TCP server with automatic hierarchical thread management.
+It accepts a user provided handler and forks dedicated thread which listens given TCP port number.
+Every time the listening thread accept a new connection from peer, it forks new thread dedicated for
+each connection then calls user provided handler.
+
+When server shutdown, all threads including listening thread, handler threads and their children
+automatically killed.  You don't have to close the socket by your own.  This module automatically
+close the socket on handler exit regardless if it exited normally or it was killed.
+You can install your own cleanup routine using finally or bracket.
+
+
+=== Example
+
+@
+newEchoServer :: IO TcpServer
+newEchoServer = newServer 8080 echoServerHandler
+
+echoServerHandler :: ThreadMap -> Socket -> IO ()
+echoServerHandler _ peer = go
+  where
+    go = do
+        msg <- recv peer 4096
+        unless (null msg) $ do
+            sendAll peer msg
+            go
+@
+
+-}
 module Network.TcpServer
     (
-      TcpServer
+    -- * Types
+      TcpHandler
+    , TcpServer
+    -- * Functions
     , newServer
     , shutdownServer
     , waitListen
+    -- * Re-exported Types
     , ThreadMap
     ) where
 
@@ -24,9 +64,33 @@ import           Network.Socket               (Family (..), PortNumber,
 import           Control.Concurrent.Hierarchy (ThreadMap, newChild,
                                                newThreadMap, shutdown)
 
+{-|
+    'TcpHandler' is type synonym of user provided function which actually working with single TCP connection.
+    Your handler is called with 'ThreadMap' coming from thread-hierarchy package.
+    You can ignore it as long as your handler doesn't create its own child thread.
+    If the handler creates child, you must use 'newChild' in thread-hierarchy with passing the given 'ThreadMap'
+    as the first argument of 'newChild' so that 'newChild' can properly install cleanup routine on exit of
+    your handler or entire server.
+-}
+type TcpHandler
+    =  ThreadMap    -- ^ Empty ThreadMap to be used when the handler creates its child thread with newChild.
+    -> Socket       -- ^ TCP socket connected from peer which the handler working with.
+    -> IO ()
+
+{-|
+    A simple plain TCP server.
+-}
 data TcpServer = TcpServer ThreadMap (MVar ())
 
-newServer :: PortNumber -> (ThreadMap -> Socket -> IO ()) -> IO TcpServer
+{-|
+    Create a new 'TcpServer'.  It forks a new thread to listen given TCP port.
+    It calls user provided 'TcpHandler' every time it accepts new TCP connection from peer.
+    The 'TcpHandler' is executed by its dedicated thread.
+-}
+newServer
+    :: PortNumber   -- ^ TCP port number the newly created server to listen.
+    -> TcpHandler   -- ^ User provided function handling each TCP connection.
+    -> IO TcpServer -- ^ newSever returns created server object.
 newServer port handler = do
     readyToConnect <- newEmptyMVar
     rootThreadMap <- newThreadMap
@@ -34,9 +98,16 @@ newServer port handler = do
         bracket (newListener port readyToConnect) close (acceptLoop listenerChildren handler)
     return $ TcpServer rootThreadMap readyToConnect
 
+{-|
+    Wait for given 'TcpServer' ready to accept new connection from peer.
+    This is useful for unit testing where you can synchronize client connection to server boot up.
+-}
 waitListen :: TcpServer -> IO ()
 waitListen (TcpServer _ readyToConnect) = readMVar readyToConnect >>= \_ -> return ()
 
+{-|
+    Shutdown given 'TcpServer'.  It kills all threads of its children, grandchildren and so on.
+-}
 shutdownServer :: TcpServer -> IO ()
 shutdownServer (TcpServer rootThreadMap _) = shutdown rootThreadMap
 
